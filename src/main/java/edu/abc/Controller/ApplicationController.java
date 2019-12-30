b2ccd3702a093eb206c557020f913b86046f356a
BLUESKY <jianguo_10@163.com>
2019-12-30 10:58:22 +0800

    前端加入添加申请流程表格

--- src/main/java/edu/abc/Controller/ApplicationController.java
+++ src/main/java/edu/abc/Controller/ApplicationController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -31,6 +32,7 @@ public class ApplicationController {
     }
 
     //添加申请
+    /*//自己写的
     @RequestMapping(value = "insertOneApplication")
     public String insertOneApplication() throws ParseException {
         String strStaDate = "2019-12-28 10:11:30";
@@ -44,6 +46,42 @@ public class ApplicationController {
         application.setTheme("请假");
         application.setContent("累了");
         application.setApprover("李华");
+        application.setStaTime(staTime);
+        application.setApprTime(apprTime);
+        applicationMapper.insertOneApplication(application);
+        return "Welcome.html";
+    }*/
+    /*//佰翰写的
+    @RequestMapping(value = "addApplication")
+    public String addApplication(HttpServletRequest request)
+    {
+        Application application = new Application();
+        application.setId(request.getParameter("id"));
+        application.setApplicant(request.getParameter("applicant"));
+        application.setTheme(request.getParameter("theme"));
+        application.setContent(request.getParameter("content"));
+        application.setApprover(request.getParameter("approver"));
+        application.setStaTime(request.getParameter("staTime"));
+        application.setApprTime(request.getParameter("apprTime"));
+        int i = applicationMapper.addApplication(application);
+        System.out.println(i);
+        return "ApplicationList.html";
+    }*/
+
+    //添加申请
+    @RequestMapping(value = "addApplication")
+    public String addApplication(HttpServletRequest request) throws ParseException {
+        Application application = new Application();
+        String strStaDate = request.getParameter("staTime");
+        String strApprDate = request.getParameter("apprTime");
+        String pat = "yyyy-MM-dd HH:mm:ss";
+        SimpleDateFormat sdf = new SimpleDateFormat(pat);
+        Date staTime = sdf.parse(strStaDate);
+        Date apprTime = sdf.parse(strApprDate);
+        application.setApplicant(request.getParameter("applicant"));
+        application.setTheme(request.getParameter("theme"));
+        application.setContent(request.getParameter("content"));
+        application.setApprover(request.getParameter("approver"));
         application.setStaTime(staTime);
         application.setApprTime(apprTime);
         applicationMapper.insertOneApplication(application);
